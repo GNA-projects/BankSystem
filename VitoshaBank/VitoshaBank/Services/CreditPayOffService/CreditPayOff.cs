ab4b58554cf90a5278cf3d9be4acc88794b69300
Alexander Dimitrov <alexander_dimitrov@yahoo.com>
2021-01-29 15:11:56 +0200

    added 2 new services

---
+++ VitoshaBank/VitoshaBank/Services/CreditPayOffService/CreditPayOff.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VitoshaBank.Data.MessageModels;
+using VitoshaBank.Data.Models;
+
+namespace VitoshaBank.Services.CreditPayOffService
+{
+    public class CreditPayOff : ControllerBase
+    {
+        public async Task<ActionResult<MessageModel>> GetCreditPayOff(Credits credit, MessageModel messageModel, BankSystemContext _context)
+        {
+            if (DateTime.Now == credit.PaymentDate)
+            {
+                if (credit.Instalment <= credit.Amount)
+                {
+                    credit.Amount = credit.Amount - credit.Instalment;
+                    credit.CreditAmountLeft = credit.CreditAmountLeft - credit.Instalment;
+                    credit.PaymentDate = DateTime.Now.AddMonths(1);
+                    await _context.SaveChangesAsync();
+                    messageModel.Message = "Credit instalment payed off successfully from Credit Account!";
+                    return StatusCode(200, messageModel);
+                }
+                else
+                {
+                    if (credit.Instalment<=credit.User.BankAccounts.Amount)
+                    {
+                        credit.User.BankAccounts.Amount = credit.User.BankAccounts.Amount - credit.Instalment;
+                        credit.CreditAmountLeft = credit.CreditAmountLeft - credit.Instalment;
+                        credit.PaymentDate = DateTime.Now.AddMonths(1);
+                        await _context.SaveChangesAsync();
+                        messageModel.Message = "Credit instalment payed off successfully from Bank Account!";
+                        return StatusCode(200, messageModel);
+                    }
+                    else
+                    {
+                        messageModel.Message = "You don't have enough money to pay off Your instalment! Come to our office as soon as possible to discuss what happens from now on!";
+                        return StatusCode(406, messageModel);
+                    }
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ VitoshaBank/VitoshaBank/Services/DepositPayentService/DividentPayment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VitoshaBank.Data.MessageModels;
+using VitoshaBank.Data.Models;
+using VitoshaBank.Services.CalculateDividendService;
+
+namespace VitoshaBank.Services.DepositPayentService
+{
+    public class DividentPayment :ControllerBase
+    {
+        public async Task<ActionResult<MessageModel>> GetDividentPayment(Deposits deposit, MessageModel messageModel, BankSystemContext _context)
+        {
+            if (DateTime.Now == deposit.PaymentDate)
+            {
+                var dividentAmount = CalculateDivident.GetDividentAmount(deposit.Amount, deposit.Divident,deposit.TermOfPayment);
+                deposit.Amount = deposit.Amount + dividentAmount;
+                deposit.PaymentDate.AddMonths(deposit.TermOfPayment);
+                await _context.SaveChangesAsync();
+                messageModel.Message = "Deposit divident applied successfully!";
+                return StatusCode(200, messageModel);
+            }
+            messageModel.Message = "Today is not payment date";
+            return StatusCode(400, messageModel);
+        }
+    }
+}
