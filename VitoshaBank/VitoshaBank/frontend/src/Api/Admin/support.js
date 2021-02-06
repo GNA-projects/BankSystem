7d30aad8e12f41431e76d259c41cd2519771026b
Gosho Trapov <goshotrapov@gmail.com>
2021-02-06 13:28:14 +0200

    quick fix 1.2.2.2 alpha

--- VitoshaBank/VitoshaBank/frontend/src/Api/Admin/support.js
+++ VitoshaBank/VitoshaBank/frontend/src/Api/Admin/support.js
@@ -4,8 +4,19 @@ export const getAllTickets = () => {
 	return viaxios
 		.get("/api/admin/get/support")
 		.then((res) => {
-			alert(res.data.message);
+			console.log(res.data[0].id)
 			return res.data;
 		})
 		.catch((err) => alert(err.response.data.message));
 };
+
+export const getAllTickets = () => {
+	return viaxios
+		.get("/api/admin/get/support")
+		.then((res) => {
+			console.log(res.data[0].id)
+			return res.data;
+		})
+		.catch((err) => alert(err.response.data.message));
+};
+
--- VitoshaBank/VitoshaBank/frontend/src/pages/Admin/User/Support/index.js
+++ VitoshaBank/VitoshaBank/frontend/src/pages/Admin/User/Support/index.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useEffect } from "react";
 import { getAllTickets } from "../../../../Api/Admin/support";
 
 export default function Support() {
-	const [tickets, setTickets] = useState();
+	const [tickets, setTickets] = useState([]);
+
+	const getTickets = async () => {
+		const array = await getAllTickets();
+		setTickets(array);
+		return array;
+	};
 	useEffect(() => {
-		setTickets(getAllTickets())
+		getTickets();
 	}, []);
 
-	return <div>123</div>;
+	return (
+		<div>
+			{tickets.map((item, i) => {
+				return (
+					<div>
+						<h1>{item.title}</h1>
+						<p>{item.id}</p>
+						<p>{item.userId}</p>
+						<p>{item.message}</p>
+						<button onClick={() => {}}></button>
+					</div>
+				);
+			})}
+			<button onClick={}>cc</button>
+		</div>
+	);
 }
